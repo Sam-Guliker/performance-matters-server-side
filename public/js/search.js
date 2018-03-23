f449f3fce73ba0e5582e5f99a8720deb75a3caaf
Sam-Guliker <samguliker@hotmail.com>
2018-03-23 10:04:05 +0100

    search update

--- public/js/search.js
+++ public/js/search.js
@@ -4,23 +4,20 @@
 
   container.prepend(searchItem)
 
-  searchItem.addEventListener('keyup', function(e){
-    console.log(e.target.value)
-  	let filter, ul, li, a, i;
+  searchItem.addEventListener('keyup', function(e) {
+    	let filter, ul, li, a, i;
 
-  	filter = searchItem.value.toLowerCase();
-  	li = container.getElementsByTagName('li');
+    	filter = searchItem.value.toLowerCase();
+    	li = container.getElementsByTagName('li');
 
-  	for (i = 0; i < li.length; i++) {
-  		a = li[i].getElementsByTagName("a")[0];
-  		if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
-  			li[i].style.display = "";
-  		} else {
-  			li[i].style.display = "none";
-  		}
-  	}
+    	for (i = 0; i < li.length; i++) {
+    		a = li[i].getElementsByTagName("a")[0];
+    		if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
+    			li[i].style.display = "";
+    		} else {
+    			li[i].style.display = "none";
+    		}
+    	}
     })
-
-
 
 }())
